34cf80e1615d1f4297509eb57f436468ff0c84f6
MrStower <therookie2012@gmail.com>
2017-03-25 19:25:29 +0700

    0.0.0

---
+++ watch/main.c
@@ -0,0 +1,19 @@
+/*
+ * watch.c
+ *
+ * Created: 25.03.2017 19:24:16
+ * Author : thero
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		//what
+    }
+}
+
